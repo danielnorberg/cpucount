dcf653621bd546f67550f4b68235f67ed9ee1f2a
Daniel Norberg <dano@spotify.com>
2014-06-06 19:10:12 -0400

    print more resources

--- src/test/java/cpucout/CpuCountTest.java
+++ src/test/java/cpucout/CpuCountTest.java
@@ -1,6 +1,10 @@
 package cpucout;
 
+import com.sun.management.OperatingSystemMXBean;
+
 import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
 
 import static java.lang.System.out;
 
@@ -8,6 +12,22 @@ public class CpuCountTest {
 
   @Test
   public void test() throws Exception {
-    out.println("availableProcessors: " + Runtime.getRuntime().availableProcessors());
+    out.println("Runtime.getRuntime().availableProcessors(): " +
+                Runtime.getRuntime().availableProcessors());
+
+    out.println("ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors(): " +
+                ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors());
+
+    final OperatingSystemMXBean operatingSystemMXBean =
+        (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+    out.println("operatingSystemMXBean.getTotalPhysicalMemorySize(): " +
+                operatingSystemMXBean.getTotalPhysicalMemorySize());
+
+    out.println("operatingSystemMXBean.getFreePhysicalMemorySize(): " +
+                operatingSystemMXBean.getFreePhysicalMemorySize());
+
+    out.println("operatingSystemMXBean.getTotalSwapSpaceSize(): " +
+                operatingSystemMXBean.getTotalSwapSpaceSize());
   }
 }
